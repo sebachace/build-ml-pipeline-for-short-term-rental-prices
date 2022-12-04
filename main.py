dcfa9efe1c77f2bc58eaa1e9a95e047ed6b45053
Sebastián Chace <seba.chace@gmail.com>
2022-12-04 01:01:02 +0100

    split commit

--- main.py
+++ main.py
@@ -40,32 +40,52 @@ def go(config: DictConfig):
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
+                version = 'main',
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
                     "artifact_type": "raw_data",
                     "artifact_description": "Raw file as downloaded"
-                },
+                }
             )
-
+        # We replicate the structure for all runs 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+            "main",
+            parameters={
+                "input_artifact": "sample.csv:latest",
+                "output_artifact": "clean_sample.csv",
+                "output_type": "clean_sample",
+                "output_description": "Data with outliers and null values removed",
+                "min_price": config['etl']['min_price'],
+                "max_price": config['etl']['max_price']
+            })
 
+        # We replicate the structure for all runs 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": config['data_check']['csv'],
+                    "ref": config['data_check']['ref'],
+                    "kl_threshold": config['data_check']['kl_threshold'],
+                    "min_price": config['data_check']['min_price'],
+                    "max_price": config['data_check']['max_price'],
+                })
+        # We replicate the structure for all runs 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
+            _ = mlflow.run(
+            f"{config['main']['components_repository']}/train_val_test_split",
+            'main',
+            version = 'main',
+            parameters={
+                'input': config['data_split']['input'],
+                'test_size': config['data_split']['test_size'],
+                'random_seed':config['data_split']['random_seed'],
+            })
+        # We replicate the structure for all runs 
         if "train_random_forest" in active_steps:
 
             # NOTE: we need to serialize the random forest configuration into JSON
@@ -74,19 +94,33 @@ def go(config: DictConfig):
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            # Again, copy the structure from the first step
+             _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                'main',
+                parameters={
+                    'trainval_artifact': config['modeling']['input'],
+                    'val_size': config['modeling']['val_size'],
+                    'stratify_by': config['modeling']['stratify_by'],
+                    'rf_config': rf_config,
+                    'max_tfidf_features':config['modeling']['max_tfidf_features'],
+                    'output_artifact':'random_forest_export',
+                }
+            )
 
         if "test_regression_model" in active_steps:
+            # Again, copy the structure from the first step
+            if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
+                _ = mlflow.run(
+                    f"{config['main']['components_repository']}/test_regression_model",
+                    'main',
+                    version = 'main',
+                    parameters={
+                        'mlflow_model': config['test_model']['mlflow_model'],
+                        'test_dataset': config['test_model']['test_dataset'],
+                    }
+                )
 
             pass
 
--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,43 +1,115 @@
 #!/usr/bin/env python
+
+
 """
-cleans and saves the resulsts
+Get data from wandb and save in temporary path,
+ apply basic cleaning and reupload to wandb
+Author: Sebastian Chace
+Data: Dec 2022
 """
+
 import argparse
 import logging
+import os
 import wandb
+import pandas as pd
+# libraries ordered via pylint
 
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logging.basicConfig(level=logging.INFO,
+                     format= "%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
-def go(args):
+def main_f(args):
 
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    # fetch file from wandb
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    dataframe = pd.read_csv(artifact_local_path, index_col="id")
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = dataframe['price'].between(min_price, max_price)
+    dataframe = dataframe[idx].copy()
+    logger.info("price: Remove outliers beyond range: %s-%s",
+                 args.min_price, args.max_price)
+    dataframe['last_review'] = pd.to_datetime(dataframe['last_review'])
+    logger.info("last_review: Correct data type")
+    # drop rows with nonsense geolocation
+    idx = dataframe['longitude'].between(-74.25, -73.50) & dataframe['latitude'].between(40.5, 41.2)
+    dataframe = dataframe[idx].copy()
+
+    # saving artifact locally
+    tmp_artifact_path = os.path.join(args.tmp_directory, args.output_artifact)
+    dataframe.to_csv(tmp_artifact_path, index=False)
+    logger.info("Temporary artifact saved to %s" , tmp_artifact_path)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
+    )
+
+    # saving artifact to wandb
+    artifact.add_file(tmp_artifact_path)
+    run.log_artifact(artifact)
+
+    artifact.wait()
+    logger.info("Clean dataset uploaded to wandb")
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="this steps cleans the data")
-
+    # We're going to construct a parser by argument, and then compile them and call our main_f function
+    parser = argparse.ArgumentParser(description="Perform basic data cleaning")
 
     parser.add_argument(
-        "--parameter_list", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--tmp_directory",
+        type=str,
+        help="Temporary directory locally",
+        required=True
+    )
+    parser.add_argument(
+        "--input_artifact",
+        type=str,
+        help="Input artifact name",
+        required=True
+    )
+    parser.add_argument(
+        "--output_artifact",
+        type=str,
+        help="Output artif. name",
+        required=True)
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        help="Output artif. type",
+        required=True)
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="Output artif. description",
+        required=True)
+
+    parser.add_argument(
+        "--min_price",
+        type=int,
+        help="Min. price limit",
         required=True
     )
 
+    parser.add_argument(
+        "--max_price",
+        type=int,
+        help="Max. price limit",
+        required=True
+    )
 
     args = parser.parse_args()
 
-    go(args)
+    main(args)
+
+    main_f(args)
